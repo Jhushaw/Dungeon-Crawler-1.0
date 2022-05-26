07a1200d2ef94960d99d1f9a3411a9111b842725
Jhushaw <35616410+Jhushaw@users.noreply.github.com>
2022-05-25 22:26:49 -0700

    implementing room and player prefabs, movement, and colliders

--- Assets/Scripts/Player Scirpts/PlayerMovement.cs
+++ Assets/Scripts/Player Scirpts/PlayerMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public Rigidbody2D rb;
     public Vector2 speed = new Vector2(50, 50);
+    private Vector3 movement;
 
     // Update is called once per frame
     void Update()
@@ -13,8 +14,7 @@ public class PlayerMovement : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
+        this.movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+        this.rb.velocity = new Vector2(movement.x, movement.y);
     }
 }
